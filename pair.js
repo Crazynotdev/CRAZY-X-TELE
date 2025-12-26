d3ca15910be7b0585a33be27bf159985c6cd875e
ZynXx <techigwe@gmail.com>
2025-12-26 22:51:07 +0100

    Update pair.js: await socket creation and guard requestPairingCode

--- pair.js
+++ pair.js
@@ -1,10 +1,19 @@
 import { createSocket } from './sock.js';
 
 export async function createPairing(number) {
-    const crazydev = createSocket(number);
-    if (!crazydev.authState.creds.registered) {
+    // createSocket is async so we must await it
+    const crazydev = await createSocket(number);
+
+    // Protect against missing authState shape
+    const registered = crazydev?.authState?.creds?.registered;
+
+    if (!registered) {
+        if (typeof crazydev.requestPairingCode !== 'function') {
+            throw new Error('Socket does not support requestPairingCode.');
+        }
         const code = await crazydev.requestPairingCode(number);
         return code;
     }
+
     throw new Error('Number is already registered.');
 }
